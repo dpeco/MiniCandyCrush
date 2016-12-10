583a32e349c506093774a19ce8379b37bb1efe3a
Yosm <Yosm@DOGE>
2016-12-10 15:38:03 +0100

    xml document

--- src/CandyCrush/main.cc
+++ src/CandyCrush/main.cc
@@ -1,10 +1,11 @@
-/******************************************************************
+/*****************************************************************
 * Copyright (C) 2016 Jordi Serrano Berbel <jsberbel95@gmail.com> *
 * This can not be copied, modified and/or distributed without    *
 * express permission of the copyright owner.                     *
 ******************************************************************/
 
 #include "GameEngine.hh"
+#include "../inc/XML/rapidxml.hpp"
 
 // Game constants
 #define SCREEN_WIDTH 1024
@@ -12,5 +13,7 @@
 
 int main(int argc, char* args[]) {
 	GameEngine::Run<SCREEN_WIDTH, SCREEN_HEIGHT>("ENTI Crush");
+	rapidxml::xml_document<> doc;
+
 	return 0;
 }
